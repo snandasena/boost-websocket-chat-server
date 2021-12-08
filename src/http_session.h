ae228e29352a18a056b49cb22d67a7972d0a8245
snandasena <sajithgithub@gmail.com>
2021-12-08 19:12:24 +0530

    Improved

--- src/http_session.h
+++ src/http_session.h
@@ -5,9 +5,32 @@
 #ifndef BOOSTCHATSERVER_HTTP_SESSION_H
 #define BOOSTCHATSERVER_HTTP_SESSION_H
 
+#include "net.h"
+#include "beast.h"
+#include "shared_state.h"
+#include <boost/optional.hpp>
+#include <boost/smart_ptr.hpp>
+#include <memory>
+#include <cstdlib>
 
 class http_session
 {
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_;
+    boost::shared_ptr<shared_state> state_;
+
+    boost::optional<http::request_parser<http::string_body>> parser_;
+
+    struct send_lambda;
+
+    void fail(beast::error_code ec, char const *what);
+    void do_read();
+    void on_read(beast::error_code ec, std::size_t);
+    void on_write(beast::error_code ec, std::size_t, bool close);
+
+public:
+    http_session(tcp::socket socket, boost::shared_ptr<shared_state> const &state);
+    void run();
 
 };
 
--- src/listener.h
+++ src/listener.h
@@ -5,10 +5,29 @@
 #ifndef BOOSTCHATSERVER_LISTENER_H
 #define BOOSTCHATSERVER_LISTENER_H
 
+#include "beast.h"
+#include "net.h"
+#include <boost/smart_ptr.hpp>
+#include <memory>
+#include <string>
 
-class listener
+// Forward declaration
+class shared_state;
+
+class listener : public boost::enable_shared_from_this<listener>
 {
+    net::io_context ioc_;
+    tcp::acceptor acceptor_;
+    boost::shared_ptr<shared_state> state_;
 
+    void fail(beast::error_code ec, char const *what);
+    void on_accept(beast::error_code ec, tcp::socket socket);
+
+public:
+    listener(net::io_context ioc, tcp::endpoint endpoint, boost::shared_ptr<shared_state> state);
+
+    // Start accepting incoming requests
+    void run();
 };
 
 
